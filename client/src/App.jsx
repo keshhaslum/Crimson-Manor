b82cb8192f80cc83872ad86fdc55d65a3e8d61a5
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-07-14 14:58:15 -0600

    Fixed the bug on rendering clues. Now it is working.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route, Link } from "react-router-dom";
-import Crime from "./components/Crime";
-import Scene from "./components/Scene";
-import Character from "./components/Character";
-import Instructions from "./components/Instructions";
-//import Room1 from "./components/Room1";
-//import Room2 from "./components/Room2";
-//import Room3 from "./components/Room3";
-import Finalpage from "./components/Finalpage";
+import { useState } from 'react';
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Crime from './components/Crime';
+import Scene from './components/Scene';
+import Character from './components/Character';
+import Instructions from './components/Instructions';
+import Finalpage from './components/Finalpage';
 import Homepage from './components/Homepage';
-import Rooms from "./components/Rooms";
-import data from "./mockData";
-
-
+import Rooms from './components/Rooms';
+import data from './mockData';
 
 function App() {
-  // console.log(data.allRooms[0]);
-  // console.log(data.allFakeClues[0]);
-
-return (
-<div>
-<Routes>
+  return (
+    <div>
+      <Routes>
         <Route path="/crime" element={<Crime />}></Route>
         <Route path="/scene" element={<Scene />}></Route>
-        <Route path= "/character" element={<Character />}></Route>
-        <Route path= "/instructions" element={<Instructions/>}></Route>
-        {/*<Route path= "/room1" element={<Room1/>}></Route>*/}
-        {/*<Route path= "/room2" element={<Room2/>}></Route>*/}
-        {/*<Route path= "/room3" element={<Room3/>}></Route>*/}
+        <Route path="/character" element={<Character />}></Route>
+        <Route path="/instructions" element={<Instructions />}></Route>
 
-        {/* {allRooms.map((room) => {
-          return (
-            <Route
-              key={room.id}
-              path={`/rooms${room.room}`}
-              element={<Rooms rooms={allRooms} fakeClues = {allFakeClues} murdererClues = {allMurdererClues}/>}
+        <Route path="/finalpage" element={<Finalpage />}></Route>
+        <Route path="/homepage" element={<Homepage />}></Route>
+        <Route
+          path="/rooms"
+          element={
+            <Rooms
+              allRooms={data.allRooms}
+              allFakeClues={data.allFakeClues}
+              allMurdererClues={data.allMurdererClues}
             />
-          );
-        })} */}
-        <Route path= "/finalpage" element={<Finalpage/>}></Route>
-     <Route path= "/homepage" element={<Homepage/>}></Route>
-     <Route path= "/rooms" element={<Rooms allRooms={data.allRooms} allFakeClues={data.allFakeClues}
-     allMurdererClues={data.allMurdererClues}/>}></Route>
+          }
+        ></Route>
       </Routes>
-
-</div>
-)
-
-
-
-
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Rooms.jsx
+++ client/src/components/Rooms.jsx
@@ -1,46 +1,34 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 
-export default function Rooms({allRooms, allFakeClues, allMurdererClues}) {
+export default function Rooms({ allRooms, allFakeClues, allMurdererClues }) {
   console.log(allFakeClues);
   const [currentRoom, setCurrentRoom] = useState(allRooms[0]);
-  const [room1Clues, setRoom1Clues] = useState([allFakeClues[0],allFakeClues[1], allMurdererClues[0]])
+  const [room1Clues, setRoom1Clues] = useState([
+    allFakeClues[0],
+    allFakeClues[1],
+    allMurdererClues[0],
+  ]);
 
   return (
-    <div className = "rooms-container">
+    <div className="rooms-container">
+      <div className="room-name-img">
+        <h3>{`${currentRoom.room}${currentRoom.img}`}</h3>
+      </div>
 
-    <div className= "room-name-img"> 
-      <h3>{`${currentRoom.room}${currentRoom.img}`}</h3>
+      <div className="room-selected-clue-img">
+        <p></p>
+      </div>
 
-      </div>  
+      <div className="room-selected-clue-description"></div>
 
-    <div className= "room-selected-clue-img">
-      
-      <p></p>
-      </div> 
-    
-
-
-    <div className= "room-selected-clue-description"></div>  
-
-    <div className= "room-clues-container">
-      
-      {room1Clues.map((clue) => {
-        <div key = {clue.id}>
-          <p>{clue.img}</p>
-        </div>
-      })}
-
-      </div>  
-    
-    
-
-
+      <div className="room-clues-container">
+        {room1Clues.map((clue) => (
+          <div key={clue.id}>
+            <p>{clue.img}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    
-
-  
-
-
-  )
+  );
 }
