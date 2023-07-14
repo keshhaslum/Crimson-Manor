08136866b2f67d57c01ef923e80e1e822e3f7018
Your Name <you@example.com>
2023-07-14 20:59:40 +0100

    addingclues

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -11,18 +11,40 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 
+function getFakeClues () {
+ 
+  const [fakeClues, setFakeClues] = useState([]);
+
+  useEffect(() => {
+    getAllFakeClues();
+  }, []);
+
+
+
+}
+
+
 export default function Instructions() {
   return (
     <div>
         <h2>Game Instructions</h2>
 
-    
+
+    <p> <h3 classname="cluesexample">
+      Your chosen clues will be stored here.</h3></p>
+
+   <p></p>
+
+
+
+
+
 
 <Link to={`/character/`}>
 <button>Previous
 </button></Link>
 
-<Link to={`/room1/`}>
+<Link to={`/rooms/`}>
 <button>Next
 </button></Link>
     
