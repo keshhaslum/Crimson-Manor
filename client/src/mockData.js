d3d959bd54089bc494104db702b02ae900ccbe5c
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-07-13 22:38:41 -0600

    Added mock data

---
+++ client/src/mockData.js
@@ -0,0 +1,112 @@
+const murderer = 3;
+
+const allCharacters = [
+  {
+    id: 1,
+    name: 'Emily',
+    last_name: 'Green',
+    description: `Emily is the victim's ambitious assistant, determined to climb the corporate ladder. She has been working for John Parker for years, but rumors suggest she was growing impatient with his management style.`,
+    what_happened: `Emily Green's ambition drove her to murder John Parker. She used the signed document to access his personal accounts and uncovered a secret business deal that could ruin her reputation. Fearing exposure, she sent the threatening letter to scare him away. On the night of the murder, she attended the party in the ballroom, wearing the cufflinks to establish an alibi. Later, she followed John to the study, where she shot him and left the murder weapon behind to frame someone else.`,
+    img: '👩🏻‍🦰',
+  },
+  {
+    id: '2',
+    name: 'Michale',
+    last_name: 'Turner',
+    description: `Michael is the victim's long-time business rival, always trying to outsmart John in the corporate world. They had a history of bitter competition.`,
+    what_happened: `Michael Turner's envy of John Parker's success pushed him over the edge. He planned to sabotage John's business deal, using the burner phone to coordinate the attack. He confronted John in the study, leading to a heated argument. In a fit of rage, he shot John and fled the scene, leaving behind the disguised costume as a diversion.`,
+    img: '👨🏻',
+  },
+  {
+    id: '3',
+    name: 'Isabella',
+    last_name: 'Martínez',
+    description: `Isabella is the victim's estranged daughter, who resented her father for prioritizing business over family. They had not spoken for years`,
+    what_happened: `Isabella Martinez's deep-seated resentment towards her father led her to commit the crime. She longed for a reconciliation but felt ignored by John. She confronted him in the study, expressing her frustration and anger. The argument escalated, and in a moment of intense emotion, she shot him. Realizing what she had done, she fled the scene, leaving behind the locket as a painful reminder of their broken relationship. With this information, you can populate the database and generate a randomly generated murder-mystery game each time the user plays. Enjoy the game creation process!`,
+    img: '💃',
+  },
+];
+
+const rooms = [
+  {
+    id: '1',
+    room: 'study',
+    main_location: 'mansion',
+    description: `The room where the victim's body was discovered. It is elegantly furnished with antique furniture and lined with bookshelves filled with rare editions.`,
+    img: '📚',
+  },
+  {
+    id: '2',
+    room: 'ballroom',
+    main_location: 'mansion',
+    description: `A grand ballroom with a sparkling chandelier, ornate decorations, and a polished marble floor. It seems to have been recently used for a party.`,
+    img: '👯',
+  },
+  {
+    id: '3',
+    room: 'conservatory',
+    main_location: 'mansion',
+    description: `A tranquil conservatory filled with exotic plants and flowers. It offers a serene escape from the rest of the mansion.`,
+    img: '🪴',
+  },
+];
+
+const murdererClues = [
+  {
+    id: '13',
+    description: `A heartfelt letter from Isabella to her father, expressing her disappointment and longing for a relationship.`,
+    character_id: '3',
+    img: '💌👨‍👧',
+  },
+  {
+    id: '16',
+    description: `A torn photograph of Isabella and the victim during happier times.`,
+    character_id: '3',
+    img: '🖼️😔',
+  },
+  {
+    id: '18',
+    description: `A locket found in the conservatory, containing a picture of Isabella and her father.`,
+    character_id: '3',
+    img: '🏛️🔐',
+  },
+];
+
+const fakeClues = [
+  {
+    id: '0',
+    description: 'test clue',
+    character_id: 'null',
+    img: '💙',
+  },
+  {
+    id: '0',
+    description: 'test clue',
+    character_id: 'null',
+    img: '💙',
+  },
+  {
+    id: '0',
+    description: 'test clue',
+    character_id: 'null',
+    img: '💙',
+  },
+  {
+    id: '0',
+    description: 'test clue',
+    character_id: 'null',
+    img: '💙',
+  },
+  {
+    id: '0',
+    description: 'test clue',
+    character_id: 'null',
+    img: '💙',
+  },
+  {
+    id: '0',
+    description: 'test clue',
+    character_id: 'null',
+    img: '💙',
+  },
+];
--- routes/characters.js
+++ routes/characters.js
@@ -3,36 +3,35 @@ var router = express.Router();
 
 // GET all characters
 // will deliver: [{id: int, name: string, last_name: string, description: string, img: string}]
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 // GET character by ID
 // will deliver: {id: 0, name: string, last_name: string, description: string, img: string}
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 // Get all clues
 // will deliver: [{id: 0, description: string, img: string}]
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 // Get clue by ID
 // will deliver: {id: 0, description: string, img: string}
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-
-
 module.exports = router;
 
-
-// All the things that need to be queried. 
-// 1. Endpoint for all characters- return an array of objects containing all necessary fields (id, name, last_name, description, img) from characters table. Get requests: Get all characters and Get characters by id (this should return an object). 
+// All the things that need to be queried.
+// 1. Endpoint for all characters- return an array of objects containing all necessary fields (id, name, last_name, description, img) from characters table. Get requests: Get all characters and Get characters by id (this should return an object).
 // [{id: int, name: string, last_name: string, description: string, img: string}]- get characters; same for get by id, but an object
-// 2. Clues should be an array of objects 
+// 2. Clues should be an array of objects
 // 3. Rooms should be an array of objects
 // 4. At the end of the game, booleans need to go back to false/default
+
+// The murderer was randomly picked and is the character with the id 3
