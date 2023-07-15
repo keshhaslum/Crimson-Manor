bf40788e3c3c04ae3ae1d7343965114a66cbbda3
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-07-14 18:16:13 -0600

    Added the component suspects to the final page

--- client/src/components/Finalpage.jsx
+++ client/src/components/Finalpage.jsx
@@ -1,20 +1,12 @@
-//logo
-//user character emoji
-//box shows 3 added clues
-//"who committed the crime?"
-//get characters by id
-//handle event function, when you click on a subject if is murderer return "correct"
-//else return to homepage
-
-//back to start button navigate to homepage
-
 import React from 'react';
+import Suspects from './Suspects';
 import { Link } from 'react-router-dom';
 
 export default function Finalpage() {
   return (
     <div>
       <h2>Who committed the crime?</h2>
+      <Suspects></Suspects>
 
       <Link to={`/`}>
         <button>Back to start</button>
