284eb6a299af2d8dd3dcd730b1ffd247e8d67a15
Jessica Zur-Linden <jzurlinden@me.com>
2023-07-16 15:08:16 -0400

    finished endpoints v1

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const cors = require('cors'); 
+const cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var charactersRouter = require('./routes/characters');
+
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors()); 
-
+app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 /* app.use(express.static(path.join(__dirname, 'public'))); */
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/characters', charactersRouter);
+// app.use('/users', usersRouter);
 
 module.exports = app;
--- model/init_db.sql
+++ model/init_db.sql
@@ -1,3 +1,11 @@
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  room INT NOT NULL,
+  main_location VARCHAR(50) NOT NULL,
+  description TEXT NOT NULL,
+  img VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE characters (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -6,23 +14,17 @@ CREATE TABLE characters (
   is_detective TINYINT(1) NOT NULL,
   is_victim TINYINT(1) NOT NULL,
   is_suspect TINYINT(1) NOT NULL,
-  img VARCHAR(255) NOT NULL
+  img VARCHAR(255) NOT NULL,
+  room_id INT,
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
-
 
 CREATE TABLE clues (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   description TEXT NOT NULL,
   character_id INT NOT NULL,
   img VARCHAR(255) NOT NULL,
-  FOREIGN KEY (character_id) REFERENCES characters(id)
-);
-
-
-CREATE TABLE rooms (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  room INT NOT NULL,
-  main_location VARCHAR(50) NOT NULL,
-  description TEXT NOT NULL,
-  img VARCHAR(255) NOT NULL
+  room_id INT,
+  FOREIGN KEY (character_id) REFERENCES characters(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
