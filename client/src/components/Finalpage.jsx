be86f8c2b87b447dc849884fe188ee958da13177
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-07-16 13:22:34 -0600

    You can see on the final screen the popups with the clues and the suspects information

--- client/src/components/Finalpage.jsx
+++ client/src/components/Finalpage.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 import Suspects from './Suspects';
+import { useChosenClues } from '../clues';
 import { Link } from 'react-router-dom';
 
 export default function Finalpage() {
-  return (
-    <div>
-      <h2>Who committed the crime?</h2>
-      <Suspects></Suspects>
+  const chosenClues = useChosenClues();
+  const [selectedClue, setSelectedClue] = useState(null);
+  console.log(selectedClue);
 
-      <Link to={`/`}>
-        <button>Back to start</button>
-      </Link>
-    </div>
+  return (
+    <>
+      <h2>Your Clues</h2>
+      <div className="final-clues">
+        {chosenClues.chosenClues.map((clue) => (
+          <div
+            className="final-clue"
+            key={`clue${clue.id}`}
+            onClick={() => {
+              setSelectedClue(clue);
+            }}
+          >
+            <p className="circular-button">{clue.img}</p>
+          </div>
+        ))}
+        {selectedClue && (
+          <div className="popup">
+            <button
+              className="close-button"
+              onClick={() => {
+                setSelectedClue(null);
+              }}
+            >
+              ❎
+            </button>
+            <h3>{selectedClue?.img}</h3>
+            <p>{selectedClue?.description}</p>
+          </div>
+        )}
+      </div>
+      <h2>The Suspects</h2>
+      <Suspects />
+    </>
   );
 }
