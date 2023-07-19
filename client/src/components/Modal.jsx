8f90e56bbb66aded0f2c9c230379ec22959aa87f
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-07-19 13:15:58 -0600

    Added circular-button class to modal when is showing clue information

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -7,12 +7,18 @@ import './styles/modal.css';
 export default function Modal({ object, onClose }) {
   const chosenClues = useChosenClues();
 
+  // Check if the `character_id` property exists in `object`
+  const isCircleButton = !object.hasOwnProperty('name');
+  console.log(isCircleButton);
+
   return (
     <div className="modal-container">
       <p className="close-icon" onClick={onClose}>
         ❌
       </p>
-      <p className="image">{object.img}</p>
+      <p className={isCircleButton ? 'image circular-button' : 'image'}>
+        {object.img}
+      </p>
       <p className="description">{object.description}</p>
       {chosenClues.chosenClues.length === 3 && (
         <button className="guess-button">It was you!</button>
